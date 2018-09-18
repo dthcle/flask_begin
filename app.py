b7000a5837bc49a7b3c5ea937572ddfb505b49c0
dthcle's GAME <dthcle@gmail.com>
2018-09-18 19:00:46 +0800

    实现了通过数据库增删改查文章 实现了通过数据库登陆管理页面（虽然没什么乱用） 优化了界面（through piao_qie） article.py待删

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from flask import request
 # from flask import url_for
 
 from static import constant
-from article import Article
+# from article import Article
+import sqlite3
+
+# from urllib.parse import unquote
 
 app = Flask(__name__)
 info = constant.Info()
@@ -15,18 +18,28 @@ info = constant.Info()
 # 主界面
 @app.route('/')
 def index_page():
-    article_list = Article.load_articles()
-    return render_template('index.html', author=info.username, article_list=article_list
+    conn = sqlite3.connect('./data/blog.db')
+    c = conn.cursor()
+    article_list = c.execute('SELECT * FROM main.article').fetchall()
+    article_content_list = []
+    for each_article in article_list:
+        article_content_list.append(each_article[2][:20]+'......')
+    conn.close()
+    return render_template('index.html', article_list=article_list, article_content_list=article_content_list
                            , footer=Markup(info.footer), background_image_address=Markup(info.background_image_address))
 
 
 # 文章界面
 @app.route('/article/<article_name>')
 def get_article(article_name):
-    article = Article.load_article(article_name)
-    return render_template('article.html', article_title=article.title
-                           , article_content=Markup(article.content.replace('\n', '<br>'))
-                           , footer=Markup(info.footer), background_image_address=Markup(info.background_image_address))
+    conn = sqlite3.connect('./data/blog.db')
+    c = conn.cursor()
+    article = c.execute('SELECT * FROM main.article WHERE title = ?', (article_name, )).fetchone()
+    conn.close()
+    return render_template('article.html', article_title=article[1]
+                           , article_content=Markup(article[2].replace('\n', '<br>'))
+                           , footer=Markup(info.footer), background_image_address=Markup(info.background_image_address)
+                           , image_address=Markup(article[4]))
 
 
 # 登陆界面
@@ -38,9 +51,15 @@ def login():
 # 跳转界面
 @app.route('/redirect', methods=['POST'])
 def redirect_to_admin():
-    if request.form['id_username'] == info.username and request.form['id_password'] == info.password:
+    conn = sqlite3.connect('./data/blog.db')
+    c = conn.cursor()
+    if request.form['id_username'] in c.execute('SELECT username FROM user').fetchall() \
+            and request.form['id_password'] == \
+            c.execute('SELECT password FROM user WHERE username = \'?\'', (request.form['id_username'],)):
+        conn.close()
         return redirect('/dthclesama')
     else:
+        conn.close()
         return redirect('/wrong_information')
 
 
@@ -64,3 +83,46 @@ def admin():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+    # conn = sqlite3.connect('./data/blog.db')
+    # c = conn.cursor()
+
+    # create_table = '''CREATE TABLE article
+    # (
+    #     ID        int            primary key        not null  ,
+    #     title     varchar(10)                                 ,
+    #     content   text                                        ,
+    #     label     varchar(10)
+    # );
+    # '''
+
+    # create_admin_table = '''CREATE TABLE user
+    # (
+    #     ID         INT         PRIMARY KEY      NOT NULL ,
+    #     username   char(20)    NOT NULL ,
+    #     password   char(30)    NOT NULL
+    # )
+    # '''
+
+    # alter = '''ALTER TABLE article ADD COLUMN image_address char(60)'''
+
+    # insert = '''INSERT INTO main.article (ID, title, content, image_address) VALUES \
+    # (1, 'Beginning!' , '哈哈哈，第一篇博客！\n庆祝博客上线！来张色图开森一哈！' \
+    # , 'https://i.loli.net/2018/09/18/5ba0c5c461fcc.jpg')
+    # '''
+
+    # insert_admin = '''INSERT INTO main.user (ID, username, password) VALUES \
+    # (0, 'dthcle', 'dthclesama')
+    # '''
+
+    # delete = '''DELETE FROM main.article WHERE ID = 2'''
+
+    # update = '''UPDATE article SET title = 'Beginning of all' WHERE ID = 1'''
+
+    # select = '''SELECT * FROM article'''
+    # article_list = c.execute(select).fetchmany(1)
+
+    # c.execute(delete)
+    # conn.commit()
+    # conn.close()
+
--- article.py
+++ article.py
@@ -8,18 +8,18 @@ class Article:
         self.content = content
 
     def save_article(self):
-        with open('./article/'+self.title+'.art', 'wb') as art:
+        with open('./data/'+self.title+'.art', 'wb') as art:
             pickle.dump(self, art, protocol=True)
 
     @staticmethod
     def load_article(article_title):
-        with open('./article/'+article_title+'.art', 'rb') as art:
+        with open('./data/'+article_title+'.art', 'rb') as art:
             tmp_art = pickle.load(art)
         return tmp_art
 
     @staticmethod
     def load_articles():
-        os.chdir('./article')
+        os.chdir('./data')
         dirs = os.listdir(os.getcwd())
         os.chdir('..')
         return dirs
